190e5489e2b194ad771a6ec56d71d1ba99f40c2f
Mikołaj Sobolewski <mikolaj.sobolewski@zsz.bobowa.pl>
2025-11-24 15:36:23 +0100

    24.11

---
+++ szachy.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>KOŁO SZACHOWE</title>
+</head>
+<body>
+    <header>
+        <h2>Koło szachowe <em>gambit piona</em></h2>
+    </header>
+    <div class="srodek">
+    <section id="lewy">
+        <h4>Polecane linki</h4>
+        <ol>
+            <li><a href="Kwerendy/kw1.png">kwerenda1</a></li>
+            <li><a href="Kwerendy/kw2.png">kwerenda2</a></li>
+            <li><a href="Kwerendy/kw3.png">kwerenda3</a></li>
+            <li><a href="Kwerendy/kw4.png">kwerenda4</a></li>
+        </ol>
+        <img src="logo.png" alt="Logo koła">
+    </section>
+    <section id="prawy">
+        <h3>Najlepsi gracze naszego koła</h3>
+        <?php
+        
+        ?>
+    </section>
+    </div>
+    <footer>
+        <p>Stronę wykonał: Mikołaj Sobolewski</p>
+    </footer>
+</body>
+</html>
