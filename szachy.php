21443e36411f5e10e97858c4c5eff1d5d6e51596
Mikołaj Sobolewski <mikolaj.sobolewski@zsz.bobowa.pl>
2025-11-27 09:11:48 +0100

    27.11

--- szachy.php
+++ szachy.php
@@ -24,7 +24,42 @@
     <section id="prawy">
         <h3>Najlepsi gracze naszego koła</h3>
         <?php
-        
+        $con = mysqli_connect("localhost", "root", "", "szachy");
+        $zapytanie = "SELECT pseudonim, tytul, ranking, klasa FROM zawodnicy WHERE ranking > 2787 ORDER BY ranking DESC;";
+        $wynik = mysqli_query($con,$zapytanie);
+        echo'<table>';
+            echo'<tr>';
+            echo'<th>Pozycja</th>';
+            echo'<th>Pseudonim</th>';
+            echo'<th>Tytuł</th>';
+            echo'<th>Ranking</th>';
+            echo'<th>Klasa</th>';
+            echo'</tr>';
+            $pozycja = 1;
+            while ($wiersz = mysqli_fetch_array($wynik)) {
+                echo'<tr>';
+                echo'<td>'.$pozycja++.'</td>';
+                echo'<td>'.$wiersz[0].'</td>';
+                echo'<td>'.$wiersz[1].'</td>';
+                echo'<td>'.$wiersz[2].'</td>';
+                echo'<td>'.$wiersz[3].'</td>';
+                echo'</tr>';
+            }
+            echo '</table>';
+            ?>
+            <form action="szachy.php" method="post">
+               <button type="submit">Losuj nową parę graczy</button>
+            </form>
+        <?php       
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $con1 = mysqli_connect("localhost", "root", "", "szachy");
+    $zapytanie1 = "SELECT pseudonim FROM zawodnicy ORDER BY RAND() LIMIT 2;";
+    $wynik1 = mysqli_query($con1, $zapytanie1);
+    while ($wiersz1 = mysqli_fetch_array($wynik1)) {
+        echo '<h4 class="obok">' . $wiersz1['pseudonim'] . '</h4>';
+    }
+}
+?>
         ?>
     </section>
     </div>
